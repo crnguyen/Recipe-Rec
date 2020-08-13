30530ffcaa2d0cf7462219031aca54b2060b6e88
Cristina Nguyen <cristinanguyen@MacBook-Pro.local>
2020-08-13 14:34:41 -0700

    [passport] add ppConfig

---
+++ config/ppConfig.js
@@ -0,0 +1,32 @@
+const passport = require("passport");
+const localStrategy = require("passport-local").Strategy;
+const db = require("../models");
+
+//passport will serialize your info to make it easier to login 
+//-convert the user based on id
+passport.serializeUser((user, cb)=>{
+    cb(null, user.id);
+})
+
+//passport deserializeuser is going to take the id and look that up in the database
+passport.deserializeUser((id, db)=>{
+    cb(null, id)
+    .catch(cb());
+});
+
+passport.use(new localStrategy({
+    usernameField: "email",
+    passwordField: "password"
+}, (email, password, cb) =>{
+    db.user.findOne({
+        where: {email}
+    })
+    .then(user=>{
+        if (!user || !user.validPassword(password)){
+            cb(null, false);
+        } else {
+            cb(null, user);
+        }
+    })
+    .catch(cb());
+}))
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const layouts = require('express-ejs-layouts');
 const app = express();
+const session = require("express-session");
+const SECRET_SESSION = process.env.SECRET_SESSION;
 
 app.set('view engine', 'ejs');
 
@@ -9,6 +11,16 @@ app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(layouts);
+
+//secret: what we are giving back to the user to use our site / session cookie
+//resave: save the session even if it's modified, make this false
+//saveUninitialized: if its a new session, we'll save it, therefore setting this to true
+
+app.use(session({
+  secret: SECRET_SESSION,
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.get('/', (req, res) => {
   res.render('index');
